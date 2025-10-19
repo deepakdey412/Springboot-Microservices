debccfcb58943cb884f10aeb2ef28f5c922ba81d
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-10-19 12:14:21 +0530

    Update dto

--- session-01/accounts/src/main/java/com/ddey/accounts/dto/AccountDto.java
+++ session-01/accounts/src/main/java/com/ddey/accounts/dto/AccountDto.java
@@ -1,4 +1,13 @@
 package com.ddey.accounts.dto;
 
+import lombok.Data;
+
+@Data
 public class AccountDto {
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+
 }
--- session-01/accounts/src/main/java/com/ddey/accounts/dto/CustomerDto.java
+++ session-01/accounts/src/main/java/com/ddey/accounts/dto/CustomerDto.java
@@ -1,4 +1,12 @@
 package com.ddey.accounts.dto;
 
+import lombok.Data;
+
+@Data
 public class CustomerDto {
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
 }
