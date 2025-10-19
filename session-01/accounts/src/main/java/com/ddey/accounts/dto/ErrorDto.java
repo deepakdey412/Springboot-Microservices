761330f8d0cf766150501337b2d8950799d86f79
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-10-19 12:16:53 +0530

    Add DTO layer

--- session-01/accounts/src/main/java/com/ddey/accounts/dto/ErrorDto.java
+++ session-01/accounts/src/main/java/com/ddey/accounts/dto/ErrorDto.java
@@ -1,4 +1,16 @@
 package com.ddey.accounts.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ErrorDto {
+    private String apiPath;
+    private String errorCode;
+    private String errorMsg;
+    private LocalDateTime errorTime;
+
 }
--- session-01/accounts/src/main/java/com/ddey/accounts/dto/ResonseDto.java
+++ session-01/accounts/src/main/java/com/ddey/accounts/dto/ResonseDto.java
@@ -1,4 +1,11 @@
 package com.ddey.accounts.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ResonseDto {
+    private String statusCode;
+    private String statusMsg;
 }
