9e605ed33aeecec6bee8d6843d229d3c633dbc26
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-10-19 12:02:44 +0530

    Add entities

---
+++ session-01/accounts/src/main/java/com/ddey/accounts/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.ddey.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="mobile_number")
+    private String mobileNumber;
+}
