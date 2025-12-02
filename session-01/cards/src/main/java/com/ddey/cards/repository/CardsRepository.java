4147ac9bed758b67fdc33e2a75c0065fa06d0882
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-12-03 00:40:03 +0530

    Add Loan and Cards services

---
+++ session-01/cards/src/main/java/com/ddey/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.ddey.cards.repository;
+
+import com.ddey.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
